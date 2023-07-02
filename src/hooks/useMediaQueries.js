82f9f09d3f416f3a38a4530fd92a3395a0be4caa
Ryan-Zayne <phoenixastro97@gmail.com>
2023-07-02 19:58:16 +0100

    refactor: api redesign for mediaquery hook

--- src/hooks/useMediaQueries.js
+++ src/hooks/useMediaQueries.js
@@ -4,48 +4,22 @@ const mobileQuery = window.matchMedia('(max-width: 767px)');
 const tabletQuery = window.matchMedia('(min-width: 768px)');
 const desktopQuery = window.matchMedia('(min-width: 1000px)');
 
-export const useMobileQuery = () => {
-	const [isMobile, setIsMobile] = useState(() => mobileQuery.matches);
+const useMediaQuery = (screenSizeQuery) => {
+	const [isScreenSize, setIsScreenSize] = useState(() => screenSizeQuery.matches);
 
 	useEffect(() => {
-		const handleMediaQueryChange = () => setIsMobile(mobileQuery.matches);
+		const handleMediaQueryChange = () => setIsScreenSize(screenSizeQuery.matches);
 
-		mobileQuery.addEventListener('change', handleMediaQueryChange);
+		screenSizeQuery.addEventListener('change', handleMediaQueryChange);
 
-		return () => mobileQuery.removeEventListener('change', handleMediaQueryChange);
-	}, []);
+		return () => screenSizeQuery.removeEventListener('change', handleMediaQueryChange);
+	}, [screenSizeQuery]);
 
-	return isMobile;
+	return isScreenSize;
 };
 
-export const useTabletQuery = () => {
-	const [isTablet, setIsTablet] = useState(() => tabletQuery.matches);
+export const useMobileQuery = () => useMediaQuery(mobileQuery);
 
-	useEffect(() => {
-		const handleMediaQueryChange = () => setIsTablet(tabletQuery.matches);
+export const useTabletQuery = () => useMediaQuery(tabletQuery);
 
-		handleMediaQueryChange(); // Set initial state
-
-		tabletQuery.addEventListener('change', handleMediaQueryChange);
-
-		return () => tabletQuery.removeEventListener('change', handleMediaQueryChange);
-	}, []);
-
-	return isTablet;
-};
-
-export const useDesktopQuery = () => {
-	const [isDesktop, setIsDesktop] = useState(() => desktopQuery.matches);
-
-	useEffect(() => {
-		const handleMediaQueryChange = () => setIsDesktop(desktopQuery.matches);
-
-		handleMediaQueryChange(); // Set initial state
-
-		desktopQuery.addEventListener('change', handleMediaQueryChange);
-
-		return () => desktopQuery.removeEventListener('change', handleMediaQueryChange);
-	}, []);
-
-	return isDesktop;
-};
+export const useDesktopQuery = () => useMediaQuery(desktopQuery);
